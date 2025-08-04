e3881522b2cab0ad32edcc0ba84be7f3f0bdd986
Walter Developer <47099208+WalterDeveloper@users.noreply.github.com>
2025-08-04 09:58:58 +0200

    Update PhpReportType.php

--- classes/report_types/PhpReportType.php
+++ classes/report_types/PhpReportType.php
@@ -1,5 +1,4 @@
 <?php
-
 class PhpReportType extends ReportTypeBase {
 	public static function init(&$report) {
 		$report->raw_query = "<?php\n//REPORT: ".$report->report."\n".trim($report->raw_query);
